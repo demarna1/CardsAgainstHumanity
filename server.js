2250ca9309330a2b8b437f0c317e62f6d7fbc296
Noah DeMarco <noah.demarco@gmail.com>
2016-01-24 15:23:28 -0500

    v0.0.0 - Rename for heroku deployment

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/main'));
 app.use(express.static(__dirname + '/play'));
 app.use(express.static(__dirname + '/game'));
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/main/index.html');
 });
 app.get('/play', function(req, res) {
     res.sendFile(__dirname + '/play/play.html');
