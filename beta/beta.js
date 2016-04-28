55584537d32a8b137b880a3252789fc5c76cbbdf
Noah DeMarco <noah.demarco@gmail.com>
2016-04-27 21:26:33 -0400

    Advanced card animation intro

--- beta/beta.js
+++ beta/beta.js
@@ -44,7 +44,7 @@ $(function() {
         trad.x = canvas.width / 2;
         stage.addChild(trad);
 
-        /* Draw Title text */
+        /* Draw title text */
         var jText = new createjs.Text('Cards Against Humanity', 'bold 48px Helvetica', '#fff');
         var jBounds = jText.getBounds();
         jScale = canvas.width / (jBounds.width * 2);
@@ -55,35 +55,45 @@ $(function() {
         stage.addChild(jText);
 
         /* Draw sample cards */
+        var sampleScale = canvas.width/1000;
         var samples = [];
-        for (var i = 0; i < 5; i++) {
-            var sample = new createjs.Shape();
-            sample.graphics.beginFill('#fff');
-            sample.graphics.drawRoundRect(0, 0, 120, 180, 5);
+        var sample_y_pos = [-300,  -750, -250, -700, -550, -900, -400, -650, -200];
+        var sample_srote = [ -15,    20,    5,  -30,   20,   45,  -10,  -10,    5];
+        var sample_rotes = [ 0.5, -0.35, -0.2, 0.55, -0.4, -0.3, 0.25, 0.15, -0.6];
+        var sample_ydiff = [  12,    12,   13,   12,   11,   13,   12,   11,   12];
+        for (var i = 0; i < sample_y_pos.length; i++) {
+            var sample = new createjs.Container();
+            var card = new createjs.Shape();
+            card.graphics.setStrokeStyle(3);
+            card.graphics.beginStroke('#222');
+            card.graphics.beginFill('#fff');
+            card.graphics.drawRoundRect(0, 0, sampleScale * 120, sampleScale * 180, 5);
+            var text = new createjs.Text('Michelle Obama\'s Arms', 'bold 14px Helvetica', '#000');
+            text.x = 10;
+            text.y = 10;
+            text.lineWidth = sampleScale * 120 - 10;
+            sample.addChild(card, text);
             sample.regX = 60;
             sample.regY = 90;
-            sample.x = (canvas.width / 2) - (i * 120 - 240);
-            if (i % 3 == 0) {
-                sample.y = -180;
-            } else if (i % 3 == 1) {
-                sample.y = -90;
-            } else {
-                sample.y = -270;
-            }
+            sample.x = (canvas.width / 2) + (i-4) * ((sampleScale*120)/2 + 25);
+            sample.y = sample_y_pos[i];
             samples.push(sample);
-            stage.addChild(sample);
+        }
+
+        // Draw evens first, then odds on top
+        for (var i = 0; i < sample_y_pos.length; i+=2) {
+            stage.addChild(samples[i]);
+        }
+        for (var i = 1; i < sample_y_pos.length; i+=2) {
+            stage.addChild(samples[i]);
         }
 
         createjs.Ticker.setFPS(30);
         createjs.Ticker.addEventListener('tick', function() {
-            for (var i = 0; i < 5; i++) {
-                if (samples[i].y < canvas.height - 10) {
-                    if (i % 2 == 0) {
-                        samples[i].rotation += 1;
-                    } else {
-                        samples[i].rotation -= 1;
-                    }
-                    samples[i].y += 10;
+            for (var i = 0; i < sample_y_pos.length; i++) {
+                if (samples[i].y < canvas.height + 30) {
+                    samples[i].rotation += sample_rotes[i];
+                    samples[i].y += sample_ydiff[i];
                 }
             }
             stage.update();
